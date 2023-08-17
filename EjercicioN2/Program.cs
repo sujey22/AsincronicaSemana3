8ba0f53655ee5ec7b72b222a10e68560494c08aa
sujey22 <da0089032022@unab.edu.sv>
2023-08-16 21:40:46 -0600

    EjercicioN2

---
+++ EjercicioN2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Ejercicio numero 2
+
+String[] NombreCompleto = { "Merlin", "Sujey", "Duarte", "Argueta" };
+
+Console.WriteLine("Valores del arreglo: ");
+
+foreach (var i in NombreCompleto)
+{
+    Console.WriteLine(i);
+}
+
+int Tamañoarreglo = NombreCompleto.Length;
+
+Console.WriteLine($"\nTamaño del arreglo: {Tamañoarreglo}");
+
+Console.WriteLine("\nTamaño del arreglo y valores: ");
+
+for(int i = 0; i < Tamañoarreglo; i++)
+{
+    Console.WriteLine($"Posicion {i + 1}: {NombreCompleto[i]}" );
+}
