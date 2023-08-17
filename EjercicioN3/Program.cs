17590796d82edb650844047751dbbed1812a0dce
sujey22 <da0089032022@unab.edu.sv>
2023-08-16 22:07:55 -0600

    EjercicioN3

---
+++ EjercicioN3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Ejercicio numero 3
+
+String[] palabras = { "Perro", "Casa", "Cebra", "Tortuga", "Burro", "Mar", "Dedo", "Tasa", "Nube", "Sol" };
+
+Console.WriteLine("Arreglo sin ordenar: ");
+for (int i = 0; i < palabras.Length; i++)
+{
+    Console.WriteLine($"Posición {i + 1}: {palabras[i]}");
+}
+
+Array.Sort(palabras);
+
+Console.WriteLine("\nArreglo ordenado: ");
+
+foreach(string i in palabras)
+{
+    Console.WriteLine(i);
+}
