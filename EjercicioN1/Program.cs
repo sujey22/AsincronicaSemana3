ae524699e7bbd5f3b2aac55256bd61221018cc54
sujey22 <da0089032022@unab.edu.sv>
2023-08-16 21:13:21 -0600

    Ejercicio1

---
+++ EjercicioN1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Inicio del ejercicio numero 1
+
+int t;
+do
+{
+    Console.WriteLine("ingrese el tamaño del arreglo (minimo 5): ");
+} while(!int.TryParse(Console.ReadLine(), out t) || t < 5); 
+
+float[] stock = new float[t];
+
+for (int i = 0; i < stock.Length; i++)
+{
+    Console.WriteLine($"ingrese el valor para la posición: ");
+    stock[i] = float.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Valores ingresados: ");
+foreach (int i in stock)
+{
+    Console.WriteLine(i);
+}
