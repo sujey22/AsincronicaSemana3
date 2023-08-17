c952d753529084f724160ac2912fb685f1656716
sujey22 <da0089032022@unab.edu.sv>
2023-08-16 22:49:10 -0600

    EjercicioN5

---
+++ Ejercicio N5/Program.cs
@@ -0,0 +1,27 @@
+﻿// Ejercicio numero 5
+
+using System;
+
+int[] Numero = { 4, 10, 45, -67, -7, 25, 89, 64 };
+
+Console.WriteLine("Arreglo en el orden original");
+
+foreach(int i in Numero)
+{
+    Console.WriteLine(i);
+}
+
+Array.Reverse(Numero);
+Console.WriteLine("\nArreglo con Reverse");
+
+foreach (int i in Numero)
+{
+    Console.WriteLine(i);
+}
+
+Array.Sort(Numero);
+Console.WriteLine("\nArreglo con Sort");
+foreach (int i in Numero)
+{
+    Console.WriteLine(i);
+}
